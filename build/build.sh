4af4ce5f24d5ffd4f36ba34d9c5a52b1f6ba3725
João Vilaça <jvilaca@bigid.com>
2022-08-06 21:14:48 +0100

    add description

--- build/build.sh
+++ build/build.sh
@@ -8,11 +8,18 @@ cut -d'/' -f4-5 src/repos > tmp
 while IFS="" read -r p || [ -n "$p" ]
 do
   printf '%s\n' "$p"
-  JSON="$(curl https://api.github.com/repos/$p)"
+  JSON="$(curl -s https://api.github.com/repos/$p)"
   SORTED="$(echo $JSON | sort)"
   LINK="[$p](https://github.com/$p)"
   STARS="$(echo $JSON | jq .stargazers_count)"
-  echo "- $LINK $STARS" >> README.md
+  DESCRIPTION="$(echo $JSON | jq .description)"
+  if [ "${STARS}" = "null" ]
+  then
+    echo "😱 could not get the number of starts for $p"
+    exit 1
+  fi
+  echo "- $LINK" >> README.md
+  echo "$DESCRIPTION (⭐️x$STARS)" >> README.md
 done < tmp
 
 cat src/footer.md >> README.md

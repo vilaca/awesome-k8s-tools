1395cc4fcee2f65034dbe5f53b32dbab2703b2c7
João Vilaça <vilaca@users.noreply.github.com>
2022-08-06 22:18:26 +0100

    Add description (#2)
    * add description
    
    * chore: autopublish 2022-08-06T20:15:27Z
    
    * add 2 spaces at end
    
    * x subscript
    
    * chore: autopublish 2022-08-06T20:21:14Z
    
    * refactor
    
    * revert
    
    * chore: autopublish 2022-08-06T20:28:15Z
    
    * fix lb
    
    * chore: autopublish 2022-08-06T20:31:19Z
    
    * formating
    
    * chore: autopublish 2022-08-06T20:45:40Z
    
    * formating
    
    * chore: autopublish 2022-08-06T20:59:12Z
    
    * formating
    
    * chore: autopublish 2022-08-06T21:03:11Z
    
    * formating
    
    * chore: autopublish 2022-08-06T21:04:59Z
    
    * formating
    
    * chore: autopublish 2022-08-06T21:05:48Z
    
    * formating
    
    * chore: autopublish 2022-08-06T21:06:53Z
    
    * formating
    
    * chore: autopublish 2022-08-06T21:10:16Z
    
    * formating
    
    * chore: autopublish 2022-08-06T21:13:38Z
    
    * formating
    
    * chore: autopublish 2022-08-06T21:15:01Z
    
    * formating
    
    * chore: autopublish 2022-08-06T21:16:29Z
    
    Co-authored-by: João Vilaça <jvilaca@bigid.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/build.sh
+++ build/build.sh
@@ -8,11 +8,17 @@ cut -d'/' -f4-5 src/repos > tmp
 while IFS="" read -r p || [ -n "$p" ]
 do
   printf '%s\n' "$p"
-  JSON="$(curl https://api.github.com/repos/$p)"
+  JSON="$(curl -s https://api.github.com/repos/$p)"
   SORTED="$(echo $JSON | sort)"
   LINK="[$p](https://github.com/$p)"
   STARS="$(echo $JSON | jq .stargazers_count)"
-  echo "- $LINK $STARS" >> README.md
+  DESCRIPTION="$(echo $JSON | jq -r .description)"
+  if [ "${STARS}" = "null" ]
+  then
+    echo "😱 could not get the number of stars for $p"
+    exit 1
+  fi
+  printf "### $LINK\n$DESCRIPTION\n(⭐️ <sup>x</sup> $STARS)\n" >> README.md
 done < tmp
 
 cat src/footer.md >> README.md

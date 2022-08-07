70905d768e586b6403c65a8ce0dd557b5b5d337d
João Vilaça <jvilaca@bigid.com>
2022-08-07 01:15:32 +0100

    fix format

--- build/build.sh
+++ build/build.sh
@@ -18,7 +18,7 @@ do
     echo "😱 could not get the number of stars for $p"
     exit 1
   fi
-  printf "### $LINK\n$DESCRIPTION\n(⭐️ <sup>x</sup> $STARS)\n" >> README.md
+  printf "### $LINK ⭐️ <sup>x</sup> $STARS\n$DESCRIPTION\n" >> README.md
 done < tmp
 
 cat src/footer.md >> README.md

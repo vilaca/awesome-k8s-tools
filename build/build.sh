717cd553d8a2d835acb9b26a5e2b6a11a26a6aed
João Vilaça <jvilaca@bigid.com>
2022-08-07 23:14:01 +0100

    sort

--- build/build.sh
+++ build/build.sh
@@ -3,20 +3,6 @@ set -eou pipefail
 
 cp resources/header.md README.md
 
-while IFS="" read -r p || [ -n "$p" ]
-do
-  printf '%s\n' "$p"
-  JSON="$(curl -s https://api.github.com/repos/"$p")"
-  NAME="$(echo "$JSON" | jq -r .name)"
-  STARS="$(echo "$JSON" | jq .stargazers_count)"
-  DESCRIPTION="$(echo "$JSON" | jq -r .description)"
-  LINK="[$NAME](https://github.com/$p)"
-  if [ "${STARS}" = "null" ]
-  then
-    echo "😱 could not get the number of stars for $p"
-    exit 1
-  fi
-  printf '### %s <sup>⭐️ x %s</sup>\n%s\n' "$LINK" "$STARS" "$DESCRIPTION" >> README.md
-done < "$(cut -d'/' -f4-5 data/repos)"
+./source/generate.sh
 
 cat resources/footer.md >> README.md

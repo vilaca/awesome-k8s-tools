e3ea97e58d47128dcda320181969b79b32cff8bf
vilaca <jvilaca@gmail.com>
2022-08-11 22:13:03 +0100

    clean

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+rm -f tmp index sorted
+
 cp resources/header.md README.md
 
-./source/generate.sh
+./scripts/generate.sh
 
 cat resources/footer.md >> README.md

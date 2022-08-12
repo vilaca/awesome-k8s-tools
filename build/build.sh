1ddf685ab9c57fbbddbe17e029dc99205d0969f7
João Vilaça <jvilaca@bigid.com>
2022-08-12 20:01:57 +0100

    test

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,6 @@ set -eou pipefail
 
 rm -f tmp index sorted
 cp resources/header.md README.md
+./build/pull.sh
 ./build/generate.sh
 cat resources/footer.md >> README.md
--- build/generate.sh
+++ build/generate.sh
@@ -5,24 +5,7 @@ sort -fo data/repos data/repos
 
 cut -d'/' -f4-5 data/repos > tmp
 
-bash --version
-
-while IFS="" read -r p || [ -n "$p" ]
-do
-  printf 'Pulling %s\n' "$p"
-  JSON="$(curl -s https://api.github.com/repos/"$p")"
-  STARS="$(echo "$JSON" | jq .stargazers_count)"
-  if [ "${STARS}" = "null" ]
-  then
-    echo "😱 could not get the number of stars for $p"
-    echo "$JSON"
-    exit 1
-  fi
-  # printf '%s %s\n' "$STARS" "${JSON//[$'\t\r\n']}"
-  printf '%s %s\n' "$STARS" "${JSON//[$'\t\r\n']}" >> index
-done < tmp
-
-sort -nr index # | cut -d' ' -f2 > sorted
+sort -nr index | cut -d' ' -f2- > sorted
 
 cat sorted
 

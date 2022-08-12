d8a782b7eaa40b2115888a7c94fab18220f7d59b
João Vilaça <jvilaca@bigid.com>
2022-08-12 19:48:55 +0100

    test

--- build/generate.sh
+++ build/generate.sh
@@ -18,7 +18,7 @@ do
     echo "$JSON"
     exit 1
   fi
-  printf '%s %s\n' "$STARS" "${JSON//[$'\t\r\n']}"
+  # printf '%s %s\n' "$STARS" "${JSON//[$'\t\r\n']}"
   printf '%s %s\n' "$STARS" "${JSON//[$'\t\r\n']}" >> index
 done < tmp
 
@@ -26,3 +26,17 @@ sort -nr index | cut -d' ' -f2 > sorted
 
 cat sorted
 
+while IFS="" read -r JSON || [ -n "$JSON" ]
+do
+  printf 'Processing %s\n' "$JSON"
+  NAME="$(echo "$JSON" | jq -r .name)"
+  STARS="$(echo "$JSON" | jq .stargazers_count)"
+  DESCRIPTION="$(echo "$JSON" | jq -r .description)"
+  LINK="[$NAME](https://github.com/$p)"
+  if [ "${STARS}" = "null" ]
+  then
+    echo "😱 could not get the number of stars for $p"
+    exit 1
+  fi
+  printf '### %s <sup>⭐️ x %s</sup>\n%s\n' "$LINK" "$STARS" "$DESCRIPTION" >> README.md
+done < sorted

8a877336de0223430ef488e1c81c8fffb15e98d3
João Vilaça <vilaca@users.noreply.github.com>
2022-08-12 20:18:10 +0100

    add k8s monitoring tools (#13)
    * add k8s monitoring tools
    * use cache
    
    Co-authored-by: João Vilaça <jvilaca@bigid.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ build/generate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+sort -fo data/repos data/repos
+
+cut -d'/' -f4-5 data/repos > tmp
+
+sort -nr index | cut -d' ' -f2- > sorted
+
+while IFS="" read -r JSON || [ -n "$JSON" ]
+do
+  printf 'Processing %s\n' "$JSON"
+  NAME="$(echo "$JSON" | jq -r .name)"
+  FULL_NAME="$(echo "$JSON" | jq -r .full_name)"
+  STARS="$(echo "$JSON" | jq .stargazers_count)"
+  DESCRIPTION="$(echo "$JSON" | jq -r .description)"
+  LINK="[${NAME^}](https://github.com/$FULL_NAME)"
+  if [ "${STARS}" = "null" ]
+  then
+    echo "😱 could not get the number of stars for $FULL_NAME"
+    exit 1
+  fi
+  printf '### %s <sup>⭐️ x %s</sup>\n%s\n' "$LINK" "$STARS" "$DESCRIPTION" >> README.md
+done < sorted

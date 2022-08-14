17555fb79e61d3c13ceb264234023cec09171056
João Vilaça <vilaca@users.noreply.github.com>
2022-08-14 02:26:49 +0100

    Update (#19)
    * add projects
    * more topics
    * add https://github.com/kubernetes-sigs/kustomize/
    * 16 topics
    
    Co-authored-by: João Vilaça <jvilaca@bigid.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/build.sh
+++ build/build.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-cp resources/header.md README.md
 ./build/pull.sh
 ./build/generate.sh
-./build/generate-extra.sh
-cat NAV.md >> README.md
-echo "## 🎉 Top 10" >> README.md
+
+cat resources/header.md > README.md
 cat TOP.md >> README.md
-cat EXTRA.md >> README.md
-echo "## ⭐️ Misc" >> README.md
-cat ALL.md >> README.md
 cat resources/footer.md >> README.md
 rm -f tmp index sorted ALL.md TOP.md NEW.md EXTRA.md NAV.md

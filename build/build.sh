4bdfe762da7366481510c708339b8f35a18d888c
João Vilaça <jvilaca@bigid.com>
2022-08-29 00:10:53 +0100

    cleanup

--- build/build.sh
+++ build/build.sh
@@ -7,4 +7,5 @@ set -eou pipefail
 cat resources/header.md > README.md
 cat TOP.md >> README.md
 cat resources/footer.md >> README.md
-rm -f tmp index sorted ALL.md TOP.md NEW.md EXTRA.md NAV.md
+
+rm -f tmp index sorted TOP.md

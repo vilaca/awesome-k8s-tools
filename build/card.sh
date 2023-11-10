1194285ad10ad7a405bd6d32ebae9b2185535496
João Vilaça <vilaca@users.noreply.github.com>
2023-11-10 11:50:20 +0000

    testing in prod

--- build/card.sh
+++ build/card.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-printf '### %s *%s*\n' "$1" "$2"
+printf '### %s' "$1"
 printf ' [⭐️](https://github.com/%s/stargazers) %s' "$3" "$(./build/number.sh $4)"
 printf ' [🚀](https://github.com/%s/network/members) %s' "$3" "$(./build/number.sh $5)"
 printf ' [💥](https://github.com/%s/issues) %s' "$3" "$(./build/number.sh $6)"
-printf ' 🪪  %s\n\n\n' "$7"
+printf ' 🪪  %s\n' "$7"
+printf '*%s*\n\n' "$2"

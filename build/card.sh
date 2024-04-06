dbbe94499b1810fcf9e18c80640f0d068601ea9e
João Vilaça <vilaca@users.noreply.github.com>
2024-04-07 00:40:32 +0100

    Update card.sh - layout test

--- build/card.sh
+++ build/card.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-printf '### %s) %s' "$8" "$1"
+printf '### %s %s' "$8" "$1"
 printf ' [⭐️](https://github.com/%s/stargazers) %s' "$3" "$(./build/number.sh $4)"
 printf ' [🚀](https://github.com/%s/network/members) %s' "$3" "$(./build/number.sh $5)"
 printf ' [💥](https://github.com/%s/issues) %s' "$3" "$(./build/number.sh $6)"
